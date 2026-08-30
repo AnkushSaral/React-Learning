52f507d8dad42c06e96d9ffe14eea21feb53fcc0
AnkushSaral <ankusaral74@gmail.com>
2026-08-30 20:22:15 +0530

    Change project name

---
+++ projects/TodoApp-Context-API-with-local-storage/src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 >Hello its new project</h1>
+    </>
+  )
+}
+
+export default App
